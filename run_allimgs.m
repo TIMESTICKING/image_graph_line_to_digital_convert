b8e43046769ce0bb31096770d316eb1662917bcc
TIMESTICKING <731908970@qq.com>
2022-04-07 16:28:50 +0800

    add

---
+++ run_allimgs.m
@@ -0,0 +1,25 @@
+
+imgsf = dir('filter_imgs\*.jpeg');
+output_folder = './outputs/';
+
+
+if ~exist("output_folder", "dir")
+    mkdir(output_folder);
+end
+
+
+for imf=imgsf'
+    [x,y,viz]=imgPlot2digital([imf.folder '/' imf.name]);
+
+    tt = split(imf.name, '.');
+    imgname = tt{1};
+    im_out_dir = [output_folder imgname '/'];
+    if ~exist("im_out_dir", "dir")
+        mkdir(im_out_dir);
+    end
+    save([im_out_dir 'xy.mat'], "x", "y");
+    saveas(viz, [im_out_dir 'viz.jpg']);
+end
+
+
+
