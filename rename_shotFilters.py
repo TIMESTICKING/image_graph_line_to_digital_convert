d7f620e4eab172214239c6044b53297feac93d79
TIMESTICKING <731908970@qq.com>
2022-04-09 22:20:30 +0800

    edit

--- rename_shotFilters.py
+++ rename_shotFilters.py
@@ -1,10 +1,13 @@
+import glob
 import os
+import threading
+
 import cv2
 import easyocr
+import numpy as np
 
 
-def get_img_id(path):
-    reader = easyocr.Reader(['en'])
+def get_img_id(path, reader):
     file = cv2.imread(path,cv2.CV_8UC1)
 
     result = reader.readtext(file)
@@ -22,12 +25,32 @@ def get_img_id(path):
     print(f'=======found {cnt} results.')
     print(numbers)
 
-    return '.'.join(numbers)
+    confidences_maxId = np.array(confidences).argmax()
+
+    return numbers[confidences_maxId]
 
 
+def read_thread(imgs, reader):
+    reader = easyocr.Reader(['en'])
+    for im in imgs:
+        number = get_img_id(im, reader)
+        print(number)
+
+
+def main(dir):
+    imgs = glob.glob(f'{dir}/*.jpeg')
+    mididx = int(len(imgs) / 2)
+
+    t1 = threading.Thread(target=read_thread, args=(imgs[:mididx],))
+    t1.start()
+    t2 = threading.Thread(target=read_thread, args=(imgs[mididx:],))
+    t2.start()
+
+    t1.join()
+    t2.join()
 
 if __name__ == '__main__':
-    print(get_img_id('./filter_imgs/IMG_3074.jpeg'))
+    main('filter_imgs')
 
 
 
