1a8fcdd323e4ded5072a532308882b22abf2cd61
TIMESTICKING <731908970@qq.com>
2022-04-09 23:07:39 +0800

    rename and download pdf

--- filter_imgs/resize.m
+++ filter_imgs/resize.m
@@ -4,7 +4,7 @@ fs = dir('*.jpeg');
 for f=fs'
     img = imread(f.name);
     img = imrotate(img, -90);
-    img = img(1539:2565, 795:1893, :);
+%     img = img(1539:2565, 795:1893, :);
 
     imwrite(img,f.name);
 end
--- rename_shotFilters.py
+++ rename_shotFilters.py
@@ -1,14 +1,35 @@
 import glob
 import os
 import threading
-
+import warnings
+from argparse import ArgumentParser
+from main import *
 import cv2
 import easyocr
+import matplotlib.pyplot as plt
 import numpy as np
 
+all_shot_numbers = []
+
+# 顺时针旋转90度
+def RotateClockWise90(img):
+    trans_img = cv2.transpose(img)
+    new_img = cv2.flip(trans_img, 1)
+    return new_img
+
+
+# 逆时针旋转90度
+def RotateAntiClockWise90(img):
+    trans_img = cv2.transpose(img)
+    new_img = cv2.flip(trans_img, 0)
+    return new_img
 
 def get_img_id(path, reader):
-    file = cv2.imread(path,cv2.CV_8UC1)
+    file = cv2.imread(path, cv2.CV_8UC1)
+    if args.rotate == -1:
+        file = RotateAntiClockWise90(file)
+    elif args.rotate == 1:
+        file = RotateClockWise90(file)
 
     result = reader.readtext(file)
 
@@ -23,34 +44,65 @@ def get_img_id(path, reader):
             print(r)
 
     print(f'=======found {cnt} results.')
-    print(numbers)
+    # print(numbers)
 
     confidences_maxId = np.array(confidences).argmax()
 
-    return numbers[confidences_maxId]
+    return file, numbers[confidences_maxId]
 
 
 def read_thread(imgs, reader):
-    reader = easyocr.Reader(['en'])
     for im in imgs:
-        number = get_img_id(im, reader)
+        file, number = get_img_id(im, reader)
         print(number)
+        if len(number) == 0:
+            warnings.warn('no numbers detected!', UserWarning)
+            continue
+
+        all_shot_numbers.append(number)
+
+        filepath, tempfilename = os.path.split(im)
+        filename, extension = os.path.splitext(tempfilename)
+
+        newpath = f'{filepath}/num_{number}{extension}'
+        # rewrite the file
+        if args.rotate != 0:
+            cv2.imwrite(newpath, file)
+            os.remove(im)
+        else:
+            # rename the file
+            os.rename(im, newpath)
+            print(f'rename to === {newpath}')
 
 
-def main(dir):
-    imgs = glob.glob(f'{dir}/*.jpeg')
-    mididx = int(len(imgs) / 2)
+def main(dir, threadsnum=3):
+    reader = easyocr.Reader(['en'])
+    imgs = glob.glob(f'{dir}/IMG*.jpeg')
+    mididx = int(len(imgs) / threadsnum) + 1
 
-    t1 = threading.Thread(target=read_thread, args=(imgs[:mididx],))
-    t1.start()
-    t2 = threading.Thread(target=read_thread, args=(imgs[mididx:],))
-    t2.start()
+    ts = []
+    for i in range(threadsnum):
+        t1 = threading.Thread(target=read_thread, args=(imgs[i * mididx: (i+1)*mididx],reader,), daemon=True)
+        t1.start()
+        ts.append(t1)
 
-    t1.join()
-    t2.join()
+    for t in ts:
+        t.join()
+
+    # get all numbers, download pdfs
+    start('.'.join(all_shot_numbers), 'auto_download_pdfs')
+    # make a success list  for matlab
+    # todo something make a success list  for matlab
+
 
 if __name__ == '__main__':
-    main('filter_imgs')
+    parser = ArgumentParser()
+    parser.add_argument('--imgs_path', type=str, default='filter_imgs')
+    parser.add_argument('--rotate', type=int, default=0,
+                        help='-1 for AntiClockWise90; 0 for nothing; 1 for ClockWise90')
+    args = parser.parse_args()
+
+    main(args.imgs_path)
 
 
 
